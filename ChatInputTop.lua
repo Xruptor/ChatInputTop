2879e7d43bc31200a26038ebe0f085bb96a99718
Xruptor <xruptor@manaflux.com>
2011-01-04 15:14:33 -0500

    -Fixed an issue where the editbox would sometimes interfere with clicking the tabs.

--- ChatInputTop.lua
+++ ChatInputTop.lua
@@ -10,11 +10,19 @@ eventFrame:SetScript("OnEvent", function(self, event, ...)
 		end
         
 		for i = 1, NUM_CHAT_WINDOWS do
+			local n = ("ChatFrame%d"):format(i)
 			local eb = _G[("ChatFrame%dEditBox"):format(i)]
 
 			eb:ClearAllPoints()
 			eb:SetPoint("BOTTOMLEFT",  ("ChatFrame%d"):format(i), "TOPLEFT",  -5, 0)
 			eb:SetPoint("BOTTOMRIGHT", ("ChatFrame%d"):format(i), "TOPRIGHT", 5, 0)
+			
+			--when the editbox is on the top, complications occur because sometimes you are not allowed to click on the tabs.
+			--to fix this we'll just make the tab close the editbox
+			--also force the editbox to hide itself when it loses focus
+			_G[n.."Tab"]:HookScript("OnClick", function() eb:Hide() end)
+			eb:HookScript("OnEditFocusLost", function(self) self:Hide() end)
+			
 		end
 
 		eventFrame:UnregisterEvent("ADDON_LOADED")
