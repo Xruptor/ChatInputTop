8845073b80ddfb5acdb36dc1ae37ff246fe492f0
Xruptor <xruptor@manaflux.com>
2011-01-04 14:34:49 -0500

    -Apparently the event frame wasn't working for everyone. -This should have been fixed.

--- ChatInputTop.lua
+++ ChatInputTop.lua
@@ -1,12 +1,10 @@
 --I just wanted a simple mod that would display the InputBox on the top of the ChatFrame rather then the bottom
 
-local f = CreateFrame("frame","ChatInputTop",UIParent)
-f:SetScript("OnEvent", function(self, event, ...) if self[event] then return self[event](self, event, ...) end end)
+local eventFrame = CreateFrame("Frame", "ChatInputTop_EventFrame", UIParent)
+eventFrame:RegisterEvent("ADDON_LOADED")
 
-f:RegisterEvent("ADDON_LOADED")
-
-function f:ADDON_LOADED(self, event, ...)
-	if arg1 == "ChatInputTop" then
+eventFrame:SetScript("OnEvent", function(self, event, ...)
+	if event == "ADDON_LOADED" and select(1, ...) == "ChatInputTop" then
 		if( DEFAULT_CHAT_FRAME ) then
 			DEFAULT_CHAT_FRAME:AddMessage("ChatInputTop Loaded.");
 		end
@@ -19,6 +17,6 @@ function f:ADDON_LOADED(self, event, ...)
 			eb:SetPoint("BOTTOMRIGHT", ("ChatFrame%d"):format(i), "TOPRIGHT", 5, 0)
 		end
 
-		f:UnregisterEvent("ADDON_LOADED");
+		eventFrame:UnregisterEvent("ADDON_LOADED")
 	end
-end
+end)
